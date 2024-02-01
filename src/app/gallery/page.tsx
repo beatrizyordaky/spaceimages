c07a38d19474845763b9695011c6f7704cb16aaa
Beatriz <byordaky@gmail.com>
2024-01-31 22:16:08 -0300

    first commit

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,9 @@
+import { Template } from '../../components/Template'
+
+export default function GalleryPage() {
+    return (
+        <Template>
+            <h1>Gallery</h1>
+        </Template>
+    )
+}
