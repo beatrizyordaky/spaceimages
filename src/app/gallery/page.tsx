6a01aae52398050b3391c6ad5817f0c13ab2df95
Beatriz <byordaky@gmail.com>
2024-02-02 12:56:01 -0300

    set hook

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,12 +1,28 @@
+'use client'
+
 import { Template, ImageCard } from '@/components'
+import { Image } from '@/resources/image/image.resource';
+import { useImageService } from '@/resources/image/image.service'
+import { useState } from 'react';
 
 export default function GalleryPage() {
+
+    const useService = useImageService();
+    const [images, setImages] = useState<Image[]>([])
+
+    async function searchImages() {
+        const result = await useService.search();
+        setImages(result);
+        console.table(result)
+    }
+
     return (
         <Template>
+            <button className='bg-gray-500' onClick={searchImages}></button>
             <section className="grid grid-cols-3 gap-8">
-                <ImageCard name='Hubble' uploadDate='01/02/2024' src="https://science.nasa.gov/wp-content/uploads/2023/04/27946391011_d40ff10573_h-jpg.webp"/>
-                <ImageCard />
-                <ImageCard />
+            
+                <ImageCard name='{imageName}' uploadDate='01/02/2024' src=""/>
+
             </section>
         </Template>
     )
