fad268e2d99de13a16a33377d89ed4c7846cdc34
Beatriz <byordaky@gmail.com>
2024-02-01 21:20:24 -0300

    create image card

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,9 +1,13 @@
-import { Template } from '../../components/Template'
+import { Template, ImageCard } from '@/components'
 
 export default function GalleryPage() {
     return (
         <Template>
-            <h1>Gallery</h1>
+            <section className="grid grid-cols-3 gap-8">
+                <ImageCard name='Hubble' uploadDate='01/02/2024' src="https://science.nasa.gov/wp-content/uploads/2023/04/27946391011_d40ff10573_h-jpg.webp"/>
+                <ImageCard />
+                <ImageCard />
+            </section>
         </Template>
     )
 }
